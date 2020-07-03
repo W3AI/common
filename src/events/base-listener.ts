6e825aca33b503579fdee75121af487c571cd190
stefian <stefan.ianta@w3ai.net>
2020-07-03 19:33:55 -0300

    + src/events # subjects, base listener and base publisher, etc

---
+++ src/events/base-listener.ts
@@ -0,0 +1,53 @@
+import { Message, Stan } from "node-nats-streaming";
+import { Subjects } from "./subjects";
+
+interface Event {
+  subject: Subjects;
+  data: any;
+}
+
+export abstract class Listener<T extends Event> {
+  abstract subject: T["subject"];
+  abstract queueGroupName: string; // same as DurableName
+  abstract onMessage(data: T["data"], msg: Message): void; // ToDo ?! 1000 - 9000 Market error codes !!!
+  private client: Stan;
+  protected ackWait = 5 * 1000; // 5000ms = 5 seconds
+
+  constructor(client: Stan) {
+    this.client = client;
+  }
+
+  subscriptionOptions() {
+    return this.client
+      .subscriptionOptions()
+      .setDeliverAllAvailable()
+      .setManualAckMode(true)
+      .setAckWait(this.ackWait)
+      .setDurableName(this.queueGroupName);
+  }
+
+  listen() {
+    const subscription = this.client.subscribe(
+      this.subject,
+      this.queueGroupName,
+      this.subscriptionOptions()
+    );
+
+    subscription.on("message", (msg: Message) => {
+      console.log(
+        `Message received: ${this.subject} / ${this.queueGroupName}`
+      );
+
+      const parsedData = this.parseMessage(msg);
+      this.onMessage(parsedData, msg);
+    });
+  }
+
+  parseMessage(msg: Message) {
+    const data = msg.getData();
+
+    return typeof data === "string"
+      ? JSON.parse(data)
+      : JSON.parse(data.toString("utf8")); // 'utf8' in case of type buffer
+  }
+}
