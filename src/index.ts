1514ef8f3b136016d4588ff521a2aac7f37968fe
stefian <stefan.ianta@w3ai.net>
2020-07-12 14:16:03 -0300

    pre alpha updates

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-// Export errors and middlewares initially in market auth
+// Export errors, middlewares and events
 export * from "./errors/bad-request-error";
 export * from "./errors/custom-error";
 export * from "./errors/database-connection-error";
